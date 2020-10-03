d5372b40994aeaf122b402512b8b572d2a6cfaff
Aleksandr Grigorii <domosedov.dev@gmail.com>
2020-10-03 21:23:39 +0300

    create new branch

--- src/components/Form/sandbox.js
+++ src/components/Form/sandbox.js
@@ -1,29 +1,3 @@
-const subjectsField = {
-  name: 'subjects',
-  label: 'Предметы',
-  multiple: true,
-  type: 'multiselect',
-  valueType: 'object',
-  required: true,
-  options: [
-    { title: 'Русский язык', value: 234 },
-    { title: 'Математика', value: 314 }
-  ],
-  defaultValue: {}
-}
-
-const nameField = {
-  name: 'name',
-  label: 'Имя',
-  multiple: false,
-  type: 'text',
-  valueType: 'string',
-  required: true,
-  options: null,
-  defaultValue: '',
-  placeholder: ''
-}
-
 /*
 FieldType {
   name: string,
@@ -42,7 +16,7 @@ export const computeInitialState = (...fields) => {
   const initialState = {}
 
   if (fields.length) {
-    fields.forEach(field => {
+    fields.forEach((field) => {
       switch (field.type) {
         case 'select':
         case 'number':
@@ -55,7 +29,9 @@ export const computeInitialState = (...fields) => {
         }
         case 'multiselect': {
           initialState[field.name] = {}
-          field.options.forEach(option => { initialState[field.name][option.value] = false })
+          field.options.forEach((option) => {
+            initialState[field.name][option.value] = false
+          })
           break
         }
         case 'agreeButton': {
@@ -107,8 +83,9 @@ export const immerReducer = (draft, action) => {
   if (TEXT_VALUE_FIELDS.includes(action.type)) {
     draft[action.payload.name] = action.payload.value
   } else if (MULTIPLE_VALUE_FIELDS.includes(action.type)) {
-    draft[action.payload.name][action.payload.value] =
-      !draft[action.payload.name][action.payload.value]
+    draft[action.payload.name][action.payload.value] = !draft[
+      action.payload.name
+    ][action.payload.value]
   } else if (action.type === SET_FILE) {
     draft[action.payload.name] = action.payload.value
   } else if (action.type === UNSET_FILE) {
@@ -119,40 +96,3 @@ export const immerReducer = (draft, action) => {
     draft[action.payload.name] = []
   }
 }
-
-console.log(computeInitialState(nameField, subjectsField))
-
-// const generateInitialState = (...selectOptions) => {
-//   const initialState = {
-//     lastname: '',
-//     name: '',
-//     middlename: '',
-//     photo: null,
-//     gender: '',
-//     dateOfBirth: '',
-//     phone: '',
-//     email: '',
-//     area: '',
-//     rate: '0',
-//     experiance: '0',
-//     education: '',
-//     description: '',
-//     documents: [],
-//     agreeOffer: false
-//   }
-
-//   if (selectOptions.length) {
-//     for (const select of selectOptions) {
-//       if (select.multiple) {
-//         initialState[select.name] = {}
-//         for (const option of select.options) {
-//           initialState[select.name][option.value] = false
-//         }
-//       } else {
-//         initialState[select.name] = ''
-//       }
-//     }
-//   }
-
-//   return initialState
-// }
